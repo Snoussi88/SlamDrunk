770783bfa21604dbf9c447ed586cb7617aeacccd
snoussi88 <snoussikhalil88@gmail.com>
2022-08-04 13:14:45 -0400

    depth images

---
+++ test2.py
@@ -0,0 +1,108 @@
+import cv2
+import torch
+import time
+import numpy as np
+import pangolin
+
+
+
+
+# Q matrix - Camera parameters - Can also be found using stereoRectify
+Q = np.array(([1.0, 0.0, 0.0, -160.0],
+              [0.0, 1.0, 0.0, -120.0],
+              [0.0, 0.0, 0.0, 350.0],
+              [0.0, 0.0, 1.0/90.0, 0.0]),dtype=np.float32)
+
+
+# Load a MiDas model for depth estimation
+#model_type = "DPT_Large"     # MiDaS v3 - Large     (highest accuracy, slowest inference speed)
+#model_type = "DPT_Hybrid"   # MiDaS v3 - Hybrid    (medium accuracy, medium inference speed)
+model_type = "MiDaS_small"  # MiDaS v2.1 - Small   (lowest accuracy, highest inference speed)
+
+midas = torch.hub.load("intel-isl/MiDaS", model_type)
+
+# Move model to GPU if available
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+midas.to(device)
+midas.eval()
+
+# Load transforms to resize and normalize the image
+midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+
+if model_type == "DPT_Large" or model_type == "DPT_Hybrid":
+    transform = midas_transforms.dpt_transform
+else:
+    transform = midas_transforms.small_transform
+
+
+# Open up the video capture from a webcam
+cap = cv2.VideoCapture("test_kitti984.mp4")
+i = 0
+while cap.isOpened():
+
+    success, img = cap.read()
+
+    start = time.time()
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # Apply input transforms
+    input_batch = transform(img).to(device)
+
+    # Prediction and resize to original resolution
+    with torch.no_grad():
+        prediction = midas(input_batch)
+
+        prediction = torch.nn.functional.interpolate(
+            prediction.unsqueeze(1),
+            size=img.shape[:2],
+            mode="bicubic",
+            align_corners=False,
+        ).squeeze()
+
+    depth_map = prediction.cpu().numpy()
+
+    depth_map = cv2.normalize(depth_map, None, 0, 1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+
+    #Reproject points into 3D
+    points_3D = cv2.reprojectImageTo3D(depth_map, Q, handleMissingValues=False)
+
+
+    #Get rid of points with value 0 (i.e no depth)
+    mask_map = depth_map > 0.4
+
+    #Mask colors and points. 
+    output_points = points_3D[mask_map]
+    output_colors = img[mask_map]
+    print(output_colors.shape)
+
+    end = time.time()
+    totalTime = end - start
+
+    fps = 1 / totalTime
+
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+    
+    depth_map = (depth_map*255).astype(np.uint8)
+    #depth_map = cv2.applyColorMap(depth_map)
+
+
+    #cv2.putText(img, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
+    cv2.imshow('Image', depth_map)
+    cv2.imwrite("depth.png"+"_"+str(i),depth_map)
+    cv2.imwrite("rgb.png"+"_"+str(i),img)
+    #cv2.imshow('Depth Map', depth_map)
+    i+=1
+    
+    
+    
+    #Generate point cloud 
+    #create_output(output_points, output_colors, output_file)
+
+    if cv2.waitKey(2) & 0xFF == 27:
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
